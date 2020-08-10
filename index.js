f8c01efbaec2ae78164f55a2b39fbbdb2eca19bb
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-10 13:29:24 -0400

    completed cat car

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-$('.thumbnail').on('click', e => {
-    console.log(e.target);
-  });
+'use strict';
+
+// function handleClicks() {
+
+//   $('.thumbnail').on('click', event => {
+
+//     const targetImg = $(event.currentTarget).find('img').attr('src');
+//     const targetAlt =$(event.currentTarget).find('img').attr('alt');
+//     $('.hero img').attr('src',targetImg);
+//     $('.hero img').attr('alt',targetAlt);
+
   
+//     console.log(event.target);
+//   });
+// }
+
+// handleClicks()
